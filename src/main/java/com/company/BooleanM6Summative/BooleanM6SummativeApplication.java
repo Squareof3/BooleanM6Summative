2327ffba695a842f935d79d73864e92db7772ec4
Squareof3 <jared_gonzalezdees@aol.com>
2019-07-12 10:23:45 -0400

    Spring.io project files added

---
+++ src/main/java/com/company/BooleanM6Summative/BooleanM6SummativeApplication.java
@@ -0,0 +1,13 @@
+package com.company.BooleanM6Summative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooleanM6SummativeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooleanM6SummativeApplication.class, args);
+	}
+
+}
