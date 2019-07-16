f618c65705a19946e7b9f30c0d7a365eeaf9fdb6
Kaleigh Herold <kaleigh224@gmail.com>
2019-07-16 10:38:39 -0400

    Finished up customer test. Working on item.

--- src/test/java/com/company/BooleanM6Summative/dao/ItemDaoTest.java
+++ src/test/java/com/company/BooleanM6Summative/dao/ItemDaoTest.java
@@ -2,6 +2,7 @@ package com.company.BooleanM6Summative.dao;
 
 
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -44,4 +45,5 @@ public class ItemDaoTest {
     }
 
 
+    @Test
 }
